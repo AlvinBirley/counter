699102c82cf020da7933b75df8e4427d3de4f263
AlvinBirley <alvinbirley@gmail.com>
2023-11-27 11:54:08 -0600

    Add color effect to number counter

--- script.js
+++ script.js
@@ -3,22 +3,34 @@ const increaseButton = document.getElementById('slide_right')
 const resetButton = document.getElementById('slide_down')
 const decreaseButton = document.getElementById('slide_left')
 let currentCount = parseInt(h2count.innerText);
+let innerH = h2count.innerHTML
+
+decreaseButton.addEventListener('click', function(){
+    currentCount--;
+    h2count.innerHTML = currentCount;
+    return (currentCount<0) ? document.getElementById("h2count").style.color = "red"
+    : (currentCount>0) ? document.getElementById("h2count").style.color = "green"
+    : document.getElementById("h2count").style.color = "black"
+    
+    
+    })
 
 
 increaseButton.addEventListener('click', function() {
     currentCount++;
     h2count.innerHTML = currentCount;
+    h2count.innerHTML = currentCount;
+    return (currentCount<0) ? document.getElementById("h2count").style.color = "red"
+    : (currentCount>0) ? document.getElementById("h2count").style.color = "green"
+    : document.getElementById("h2count").style.color = "black"
+    
 }
-
 );
 
 resetButton.addEventListener('click', function() {
-h2count.innerHTML = 0;
-
+currentCount = 0;
+h2count.innerHTML = currentCount;
+document.getElementById("h2count").style.color = "black"
 }
 );
 
-decreaseButton.addEventListener('click', function(){
-currentCount--;
-h2count.innerHTML = currentCount
-})
